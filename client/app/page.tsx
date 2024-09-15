3313a2d86091329b7b07bb0c3606f588c01ca24a
Chris Ge <chrisge@MacBook-Pro-4.local>
2024-09-14 22:54:08 -0400

    got gmail api working through clerk

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,15 +1,39 @@
-"use client";
-import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import { google } from "googleapis";
+import ClientComponent from "./clientcomponent";
 
-export default function Home() {
-  const [message, setMessage] = useState("Loading");
-  useEffect(() => {
-    fetch("http://localhost:8080/api/train")
-      .then((response) => response.json())
-      .then((data) => {
-        setMessage(data.message);
+async function getGmailLabels() {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return { message: "User not found" };
+    }
+    const clerkResponse = await clerkClient.users.getUserOauthAccessToken(
+      userId,
+      "oauth_google"
+    );
+    if (clerkResponse.data && clerkResponse.data.length > 0) {
+      const token = clerkResponse.data[0].token;
+      const gmail = google.gmail({
+        version: "v1",
+        headers: { Authorization: `Bearer ${token}` },
       });
-  }, []);
-  return <div>{message}</div>;
+      const res = await gmail.users.labels.list({
+        userId: "me",
+      });
+      return res.data.labels;
+    } else {
+      return { message: "No OAuth token found" };
+    }
+  } catch (error) {
+    console.log("[GMAIL ERROR]", error);
+    return { message: "Internal error" };
+  }
+}
+
+export default async function Page() {
+  const gmailLabels = await getGmailLabels();
+
+  return <ClientComponent initialGmailLabels={gmailLabels} />;
 }
--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          net: false,
+        };
+      }
+      return config;
+    },
+  };
+  
+  export default nextConfig;
 
-export default nextConfig;
